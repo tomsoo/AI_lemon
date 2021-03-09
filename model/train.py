33a039e1147fb576ba9877c374d398b3ef2f3ad5
ysato <s171039@stn.nagaokaut.ac.jp>
2021-03-09 15:38:11 +0900

    add validation

--- model/train.py
+++ model/train.py
@@ -36,6 +36,10 @@ def main():
     # パラメータの読み込み
     train_file = param['train_file']
     train_folder = param['train_folder']
+
+    valid_file = param['valid_file']
+    valid_folder = param['valid_folder']
+
     batch_size = param['batch_size']
 
     trans = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
@@ -43,7 +47,8 @@ def main():
     train_dataset = LemonDataset(train_file, train_folder, trans)
     train_dataloader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
-
+    valid_dataset = LemonDataset(train_file, train_folder, trans)
+    valid_dataloader = torch.utils.data.DataLoader(dataset=valid_dataset, batch_size=batch_size, shuffle=True)
     #train_img, train_label = read_csv(train_filename)
 
     # 前処理
@@ -59,6 +64,11 @@ def main():
     # 損失関数とオプティマイザーの定義
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(cnn.parameters(), lr=0.001, momentum=0.9)
+
+    valid_total = 0
+    valid_correct = 0
+    valid_loss_list = []
+    valid_acc_list = []
 
     # 学習ループ
     for epoch in range(epoch_num):
@@ -79,6 +89,25 @@ def main():
             # 計算状態の出力
             running_loss += loss.item()
         print('epoch: %d, loss: %.5f' % (epoch + 1, running_loss))
+
+        valid_runnning_loss = 0.0
+        with torch.no_grad():
+            for data in valid_dataloader:
+                inputs, labels = data
+
+                outputs = cnn(inputs)
+
+                valid_loss = criterion(outputs, labels)
+                _, predicted = torch.max(outputs,1)
+                valid_total += labels.size(0)
+                valid_correct += (predicted == labels).sum().item()
+
+                valid_acc = valid_correct/valid_total * 100
+
+                valid_loss_list.append(valid_loss)
+                valid_acc_list.append(valid_acc)
+        print('valid loss:{}, valid acc:{}'.format(valid_loss, valid_acc))
+
 
     print("Finished Training")
 
